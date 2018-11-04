76ae2c861c4d0a80c6c4977c4e35ead59e949c16
linkhl09 <af.hernandezl@uniandes.edu.co>
2018-11-04 15:59:55 -0500

    Update data/Crear y poblar tablas/select's generales.sql

---
+++ data/Crear y poblar tablas/select's generales.sql
@@ -0,0 +1,26 @@
+
+Select * from proveedor where nit = '830047819';
+select * from proveedores_producto;
+select * from sucursal_producto;
+select * from cliente;
+select * from empresa;
+select * from personaNatural;
+select * from producto;
+select * from sucursal_producto;
+select * from sucursal;
+select * from bodega;
+select * from estante;
+
+select * from ordenPedido;
+select * from PRODUCTO_ORDENPEDIDO;
+
+
+
+
+
+
+
+
+select * from ordenPedido Where proveedor = '830047819' AND estado = 'En Espera' AND idSucursal = 1;
+
+
