c1251a2b0c1a1863156868ed416c154cbd9ab01f
linkhl09 <af.hernandezl@uniandes.edu.co>
2018-12-02 19:17:34 -0500

    Por fin el 13!!!!!!!!!!!!!

--- data/Iteracion3/RFC/RFC13.sql
+++ data/Iteracion3/RFC/RFC13.sql
@@ -1,8 +1,16 @@
-Select cliente, NumMesesCompra from (select cliente, count(distinct(month)) as NumMesesCompra from (select cliente, EXTRACT(MONTH FROM fecha) as month from factura where EXTRACT(YEAR FROM FECHA) = 2018) group by cliente)  where NumMesesCompra = 12;
-
-select cliente, ;
-(select cliente, count(numero) as numFacturas from factura group by cliente);
-
-(SELECT factura FROM FACTURA_PRODUCTO , PRODUCTO WHERE PRODUCTO.CATEGORIA ='Technology' OR PRODUCTO.CATEGORIA = 'Tools');
- 
-SELECT * FROM FACTURA_PRODUCTO , PRODUCTO WHERE PRODUCTO.PRECIOUNITARIO > 100000;
+--Los clientes que realizan una compra por lo menos una vez al mes.
+Select z.cliente, NumMesesCompra, productoCaro, productoCategorias 
+from (
+Select cliente, NumMesesCompra 
+from (
+    select cliente, count(distinct(month)) as NumMesesCompra 
+    from (
+        select cliente, EXTRACT(MONTH FROM fecha) as month 
+        from factura 
+        where EXTRACT(YEAR FROM FECHA) = 2018
+    ) group by cliente
+) where NumMesesCompra = 12)z,(SELECT a.cliente, productoCaro, productoCategorias from
+(SELECT cliente, count(numero) as numFacturas from factura group by cliente)a, 
+(SELECT cliente, count(distinct(factura))as numFacturasCaras, nombre as productoCaro FROM FACTURA, FACTURA_PRODUCTO, PRODUCTO WHERE PRODUCTO.PRECIOUNITARIO > 100000 group by cliente,nombre), 
+(SELECT cliente, count(distinct(factura))as numFacturasCat, nombre as productoCategorias FROM FACTURA, FACTURA_PRODUCTO , PRODUCTO WHERE PRODUCTO.CATEGORIA ='Technology' OR PRODUCTO.CATEGORIA = 'Tools' group by cliente, nombre)
+where numFacturas = numFacturasCaras OR numFacturas = numFacturasCat);
